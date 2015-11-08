30dcf381132c60528b3418281596772e0cdbb0df
EinBaum <mail@einbaum.org>
2015-11-08 23:04:12 +0100

    Version bump

--- SuperIgnore.lua
+++ SuperIgnore.lua
@@ -1,7 +1,7 @@
 
 local S_ADDON_NAME				= "SuperIgnore"
 local S_ADDON_DIR				= "superignore"
-local S_ADDON_VERSION			= "1.1.1"
+local S_ADDON_VERSION			= "1.1.2"
 local S_AUTO_RESPONSE			= "~Ignored~ (" .. S_ADDON_NAME .. " AddOn)"
 local S_TEXT_OPTIONS			= "Ignore Filter"
 local S_TEXT_EXTRA				= "Extra Features"
